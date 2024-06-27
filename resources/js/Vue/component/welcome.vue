3651449373c9f5574717bf91301d34402d61b196
Titouan Saint-Chamarand <titouanstchamarand@gmail.com>
2024-06-28 01:11:55 +0200

    Tailwind CSS + Doodle CSS

--- resources/js/Vue/component/welcome.vue
+++ resources/js/Vue/component/welcome.vue
@@ -1,11 +1,18 @@
 
 
 <template>
-    <div>
-        <h1 class="text-3xl font-bold underline">Welcome to Vue 3</h1>
+    <div class="doodle">
+        <h1 class="font-shortstack">Welcome to Vue 3</h1>
+        <input>
     </div>
 </template>
 
 <script setup>
 console.log('Hello from Vue 3')
 </script>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Short+Stack&display=swap');
+body {
+    font-family: 'Short Stack', cursive;
+}
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import './bootstrap';
 
 import { createApp } from 'vue';
 import welcome from './Vue/component/welcome.vue';
+import 'doodle.css/doodle.css';
 
 const app = createApp(welcome);
 app.mount('#app');
