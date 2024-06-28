850f7912d10e8956ca7d9cb5c6564c3be83b7b3c
Titouan Saint-Chamarand <titouanstchamarand@gmail.com>
2024-06-28 16:29:45 +0200

    Create + View tasks

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/tasks', [TasksController::class, 'index']);
+
+Route::get('tasks/add', [TasksController::class, 'create_view']);
+Route::post('tasks/add', [TasksController::class, 'create']);
+
+Route::get('tasks/edit/{id}', [TasksController::class, 'edit_view']);
+
